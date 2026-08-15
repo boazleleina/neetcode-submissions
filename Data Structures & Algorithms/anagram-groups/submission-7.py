7c1867ecb697297b233a81865038c882e705abad
Boaz Leleina <63369143+boazleleina@users.noreply.github.com>
2026-08-15 16:00:30 -0700

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #create the hashmap to hold the sublists- will be a dictionary of lists
+        chr_list = defaultdict(list)
+
+        #loop through the strs array and convert each strs to an integer equivalent
+
+        for wrd in strs:
+            #I need the key, create a list of 26 values of the alphabet
+            count = [0] * 26
+
+            for ch in wrd:
+                #convert characters to integer equivalent, and add them to the list
+                count[ord(ch) - ord("a")] += 1
+
+            #append the count to the hashmap
+            #list is mutable, so convert to tuple
+
+            chr_list[tuple(count)].append(wrd)
+
+        return list(chr_list.values())
