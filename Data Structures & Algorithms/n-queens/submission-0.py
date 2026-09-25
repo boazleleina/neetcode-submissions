db0190975f38f182572d772a582adcdc840e7e8a
Boaz Leleina <63369143+boazleleina@users.noreply.github.com>
2026-09-24 22:11:50 -0700

    Add: n-queens - submission-0

---
+++ Data Structures & Algorithms/n-queens/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        cols = set ()
+        posDiag = set()
+        negDiag = set()
+
+        board = [["."] * n for _ in range(n)]
+        res = []
+
+        def backtracking(row):
+            if row == n:
+                res.append(["".join(row) for row in board])
+                return
+
+            for col in range(n):
+                if col in cols or (row+col) in negDiag or (row-col) in posDiag:
+                    continue
+                board[row][col] = "Q"
+                cols.add(col)
+                posDiag.add((row-col))
+                negDiag.add((row+col))
+                backtracking(row+1)
+                board[row][col] = "."
+                cols.remove(col)
+                posDiag.remove((row-col))
+                negDiag.remove((row+col))
+
+        backtracking(0)
+        return res
+                
+        
