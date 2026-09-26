c2f0fae7644692734be12e4c46a5226d90b4923d
Boaz Leleina <63369143+boazleleina@users.noreply.github.com>
2026-09-25 23:11:12 -0700

    Add: search-for-word-ii - submission-3

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-3.py
@@ -0,0 +1,56 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = None
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+
+        def _insert(w):
+            node = root
+            for c in w:
+                if c not in node.children:
+                    node.children[c] = TrieNode()
+                node = node.children[c]
+            node.word = w
+        
+        for w in words:
+            _insert(w)
+        
+        row, col = len(board), len(board[0])
+        res = []
+        path = set()
+
+        def dfs(r, c, node):
+            #check if the character is out of bound and return False
+            if (r<0) or (r >= row) or (c<0) or (c >= col) or ((r,c) in path) or (board[r][c] not in node.children):
+                return False
+            
+            #character is valid, declare a node here
+            node = node.children[board[r][c]]
+            # check if the current node has a word
+            if node.word != None:
+                res.append(node.word)
+                #change word to None after we get it
+                node.word = None
+            #insert the current cell to the path
+            path.add((r,c))
+
+            #recurse over all four neighboard
+            dfs(r-1, c, node)
+            dfs(r+1, c, node)
+            dfs(r, c-1, node)
+            dfs(r, c+1, node)
+
+            #remove the cell from path
+            path.remove((r,c))
+           
+
+        
+        for r in range(row):
+            for c in range(col):
+                dfs(r,c,root)
+        return res
+            
+            
+        
